6ff450f63365955bfb59b62a4cdaf1fbd7e0c473
Dmitri Vassilenko <dv@glyphy.com>
2026-02-01 18:17:15 -0500

    add more cleanups, particularly statuses

--- roles/mastodon/templates/cleanup.sh
+++ roles/mastodon/templates/cleanup.sh
@@ -4,10 +4,26 @@ set -eou pipefail
 
 cd "{{ root }}"
 
-echo "starting media removal"
+echo "starting statuses cleanup"
+docker-compose exec -T web tootctl statuses remove --days 90
+
+echo "starting accounts cleanup"
+docker-compose exec -T web tootctl accounts prune
+
+echo "starting media cleanup"
+docker-compose exec -T web tootctl media remove --days 1
+
+echo "starting profiles cleanup"
 docker-compose exec -T web tootctl media remove --days 1 --prune-profiles
-echo "done media removal"
+
+echo "starting headers cleanup"
+docker-compose exec -T web tootctl media remove --days 1 --remove-headers
 
 echo "starting orphans cleanup"
 docker-compose exec -T web tootctl media remove-orphans
-echo "done orphans cleanup"
+
+echo "starting emoji cleanup"
+docker-compose exec -T web tootctl emoji purge --remote-only
+
+
+echo "done"
